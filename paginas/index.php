d91ff3276f3dc07367cfe65903bc596061bb450c
Alejandro <alejandro.ruiz.santana@ieslaarboleda.es>
2026-01-20 15:24:25 +0100

    Actualización del portafolio

---
+++ paginas/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <header>
+        <img src="" alt="">
+         <h1 class="text-3xl font-bold underline S ">Hello world!</h1>
+
+    </header>
+
+    <main>
+
+    </main>
+        <p>contenido</p>
+    <footer>
+
+    </footer>
+</body>
+</html>
